7e736a5363afe9e5f3cfa8432bcf96b2d457b835
Andrea Huallpayunca <andreahuallpayuncah@gmail.com>
2022-11-22 04:00:10 -0500

    información y evolucion

--- src/main.js
+++ src/main.js
@@ -26,30 +26,67 @@ const containerPokemons = document.querySelector('#container-card');
         <p class="poke-info"> GEN name: ${poks.generation['name']}</p>
        </div>`;
       
-    
+       card.addEventListener('click', () => {
+        const mostrar = mostrarModal(poks);
+        mostrar.classList.add('modal');
+      });
+
     count = 1;
     containerPokemons.appendChild(card);
+ 
   });
+
   document.getElementById('quantity').innerHTML = count;
   return containerPokemons;
 };
 
 tarjetasPokemones(pokemonList); //usar el metodo con la data 
 
-//1.2 Información pokemon
 
+
+//1.2 Información pokemon-modal
+
+
+const mostrarModal=(poks)=>{
 //identificar los elementos
-let nombrePokemon= data.pokemon[0].name;
+/*let nombrePokemon= data.pokemon[0].name;
 let imgPokemon=data.pokemon[0].img;
+let numero=data.pokemon[0].num;
+let generacion=data.pokemon[0].generation.num;
+let tipo= data.pokemon[0].type[0];
+let size=data.pokemon[0].size.height;
+let nextEvolucion1=data.pokemon[0].evolution['next-evolution'][0]['name'] ;
+*/
 
 //crear un nodo
+
 let modal=document.createElement('div');
  modal.innerHTML=
-`<div class="modal-flex">
-  <p class="nombre">${nombrePokemon}</p>
-  <p class="cerrar"> X </p>
-
-  <img src="${imgPokemon}">
+`<div class="cuadro">
+  <div class="modal-flex">
+    <div class="head-modal">
+      <p class="nombre">${poks.num} - ${poks.name}</p>
+      <p class="cerrar"> X </p>
+    </div>  
+    <img class="imagen" src="${poks.img}">
+    <div class="info">
+    
+      <p class="sub">Generation:</p>
+      <p class="sub">${poks.generation.num}</p>
+      <p class="sub">${poks.generation.name} </p>
+      <p class="sub">Type: ${poks.type[0]} </p>
+      <p class="sub">Height: ${poks.size.height} </p>
+      <p class="sub">Weight: ${poks.size.weight} </p>
+    </div>
+    <div class="evolucion">
+      <p class=titulo>Evolución</p>
+      <div class="evo1">
+      <p class=sub>Next evolution:${poks.evolution["next-evolution"][0].name} </p>
+      <p class="sub">Candy:${poks.evolution.candy} </p>
+      <p class="sub">Candy-cost:${poks.evolution["next-evolution"][0]["candy-cost"]} </p>
+      </div>
+      </div>
+  </div>
 </div>`
 modal.className = 'modal'
 
@@ -58,6 +95,22 @@ let elementoPadre=document.querySelector('.container-modal')
 
 //agregar nodo
 elementoPadre.appendChild(modal);
+
+modal.style.display = 'flex';
+modal.querySelector('.cerrar').addEventListener('click', () => {
+  modal.classList.remove('modal');
+  elementoPadre.innerHTML = '';
+});
+const modalF = document.querySelector('.modal');
+window.addEventListener('click', (evento) => {
+  if (evento.target === modalF) {
+    modal.classList.remove('modal');
+    elementoPadre.innerHTML = '';
+  }
+});
+return modal;
+};
+
 
 
 
